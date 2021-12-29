faac7752d36729b91630ec84f859eac59eac7f06
Narayan Prusty <arsenal.narayan@gmail.com>
2021-12-29 17:23:42 +0530

    pick unique layer hashes to detect forks

--- monitor/monitor.go
+++ monitor/monitor.go
@@ -2,12 +2,10 @@ package monitor
 
 import (
 	"github.com/spacemeshos/spacemesh-watch/monitor/state_root_hash"
-	"github.com/spacemeshos/spacemesh-watch/monitor/sync_status"
-	"github.com/spacemeshos/spacemesh-watch/monitor/verified_layer"
 )
 
 func StartMonitoring() {
-	go verified_layer.MonitorVerifiedLayerProgress()
-	go sync_status.MonitorSyncStatus()
+	// go verified_layer.MonitorVerifiedLayerProgress()
+	// go sync_status.MonitorSyncStatus()
 	go state_root_hash.MonitorStateRootHash()
 }
