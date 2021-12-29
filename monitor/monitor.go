a2a1823d239e9cb8fa93c0b79e11c7482b91a3b9
Narayan Prusty <arsenal.narayan@gmail.com>
2021-12-29 17:26:12 +0530

    undo

--- monitor/monitor.go
+++ monitor/monitor.go
@@ -2,10 +2,12 @@ package monitor
 
 import (
 	"github.com/spacemeshos/spacemesh-watch/monitor/state_root_hash"
+	"github.com/spacemeshos/spacemesh-watch/monitor/sync_status"
+	"github.com/spacemeshos/spacemesh-watch/monitor/verified_layer"
 )
 
 func StartMonitoring() {
-	// go verified_layer.MonitorVerifiedLayerProgress()
-	// go sync_status.MonitorSyncStatus()
+	go verified_layer.MonitorVerifiedLayerProgress()
+	go sync_status.MonitorSyncStatus()
 	go state_root_hash.MonitorStateRootHash()
 }
